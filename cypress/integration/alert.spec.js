2fa70e5176ea86bf2bdc0092beb211142b3548a9
Pablo <pablo.mg@hotmail.com>
2021-06-09 00:11:17 -0300

    add alert example

---
+++ cypress/integration/alert.spec.js
@@ -0,0 +1,26 @@
+///<reference types="cypress" />
+
+describe('Work with alerts', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Alert', () => {
+        cy.get('#alert').click()
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Alert Simples')
+        })
+    })
+
+    it.only('Alert com mock', ()=>{
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#alert').click().then(()=>{
+            expect(stub.getCall(0)).to.be.calledWith('Alert Simples')      })
+    })
+})
